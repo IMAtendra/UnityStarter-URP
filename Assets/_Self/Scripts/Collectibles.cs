be718d7be17b253401a7927dffe950242e51fb80
IMAtendra <66991043+IMAtendra@users.noreply.github.com>
2024-04-25 19:41:42 +0530

    Add Coin Pickup Effect
    - Update Collectibles.cs
    - Add Package "Cartoon FX Remaster"

--- Assets/_Self/Scripts/Collectibles.cs
+++ Assets/_Self/Scripts/Collectibles.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.Rendering.Universal;
@@ -20,30 +21,17 @@ public class Collectibles : MonoBehaviour, ICollectible
     public CollectibleType ItemType;
     public static event Action<int, CollectibleType> OnCollectiblesCollected;
 
-    public GameObject target;
-    public bool move;
-
-    void Awake()
-    {
-        target = GameObject.FindGameObjectWithTag("Finish");
-    }
+    public GameObject destroyEffect;
 
     public void Collect()
     {
         Debug.Log($"Hi You Have Collected {ItemType}");
         OnCollectiblesCollected?.Invoke(IncrementValue, ItemType);
         gameObject.GetComponent<Collider>().enabled = false;
-        move = true;
-    }
-
-    void LateUpdate()
-    {
-        if (move)
-        {
-            // move to Target 
-            transform.position = Vector3.Lerp(transform.position, target.transform.position, 1f * Time.deltaTime);
-            // Desired Distance to remove coin Object in scene
-            if (Vector3.Distance(transform.position, target.transform.position) < 1f) Destroy(gameObject);
-        }
+        Destroy(gameObject);
+        
+        // Spawn & Destroy Effect
+        var temp = Instantiate(destroyEffect, transform.position, quaternion.identity);
+        Destroy(temp, 2f);
     }
 }
